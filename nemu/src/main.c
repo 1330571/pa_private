40827ab680becfe16688d133974a494664d18a0e
xys <1422286969@qq.com>
2020-03-22 19:45:41 +0800

    Test

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -4,7 +4,7 @@ void ui_mainloop(int);
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
-
+  //Test for git 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
