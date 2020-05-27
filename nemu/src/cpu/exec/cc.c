5d05ca3b2529db3b2782bbeb027183f21623cd6f
xys <1422286969@qq.com>
2020-05-28 00:17:49 +0800

    PA2.2

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -2,6 +2,7 @@
 
 /* Condition Code */
 
+//JO │JNO│JB │JNB│JZ │JNZ │JBE│JNBE│JS │JNS│JP │JNP│JL │JNL │JLE│JNLE
 void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   bool invert = subcode & 0x1;
   enum {
@@ -13,20 +14,39 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
 
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
-  switch (subcode & 0xe) {
+  switch (subcode & 0xe) { // & 1110 去掉最后1位的奇偶情况
     case CC_O:
+      rtl_get_OF(dest);
+      break;
     case CC_B:
+      rtl_get_CF(dest);
+      break;
     case CC_E:
-    case CC_BE:
+      rtl_get_ZF(dest);
+      break;
+    case CC_BE: //CF or ZF
+      rtl_get_ZF(&t0);
+      rtl_get_CF(&t1);
+      rtl_or(dest,&t0,&t1);
     case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
+      rtl_get_SF(dest);
+    case CC_L: //SF != OF
+      rtl_get_SF(&t0);
+      rtl_get_OF(&t1);
+      rtl_xor(dest,&t1,&t0);
+      break;
+    case CC_LE: //ZF = 1  & SF != OF
+      rtl_get_ZF(&t0);
+      rtl_get_SF(&t1);
+      rtl_get_OF(&t2);
+      rtl_xor(&t3,&t1,&t2);
+      rtl_and(dest,&t0,&t3);
+      break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
 
   if (invert) {
-    rtl_xori(dest, dest, 0x1);
+    rtl_xori(dest, dest, 0x1); //如果对应的是另外一种情况
   }
 }
