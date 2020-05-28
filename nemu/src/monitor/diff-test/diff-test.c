cb5a31cee1b2331ddad96a5d01a3231438f88b84
xys <1422286969@qq.com>
2020-05-28 08:55:08 +0800

    PA2.2

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -170,6 +170,7 @@ void difftest_step(uint32_t eip) {
     printf("esi r=%-10x cpu=%-10x\n",r.esi,cpu.esi);
     printf("ebp r=%-10x cpu=%-10x\n",r.ebp,cpu.ebp);
 
+    printf("EFLAGS = \n%d\n%d",r.eflags,cpu.eflags);
     nemu_state = NEMU_END;
   }
 }
