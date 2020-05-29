5929020cbfeada5bdbc07eaf672e707c18e6690e
xys <1422286969@qq.com>
2020-05-29 15:26:48 +0800

    PA2.2

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -42,6 +42,9 @@ void _draw_sync() {
 int _read_key() {
   //0x60数据寄存器 0x64状态寄存器 按键出现->状态寄存器为1
   //通码 = 断码 + 0x8000
-  
-  return _KEY_NONE;
+  uint32_t key = 0;
+  if(inb(0x64)){
+    key = inl(0x60);
+  }
+  return key;
 }
