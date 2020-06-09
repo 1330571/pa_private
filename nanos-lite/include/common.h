a5c009605d727b47100a0ec01cbb6a159d25c92b
xys <1422286969@qq.com>
2020-06-09 21:03:57 +0800

    PA3

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -5,8 +5,6 @@
 #include <klib.h>
 #include "debug.h"
 
-extern void ramdisk_read(void *buf, off_t offset, size_t len);
-extern size_t get_ramdisk_size();
 typedef char bool;
 #define true 1
 #define false 0
