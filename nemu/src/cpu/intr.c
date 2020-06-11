9308c087a4d01836b35e7fb60eeed6d1d5489526
xys <1422286969@qq.com>
2020-06-11 17:01:32 +0800

    PA3

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -10,10 +10,10 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push(&cpu.eip);
   //依次入栈，有用的部分，前4位： 31..16 15  后4位： 15..0
   assert(NO*8 <= cpu.IDTR.limit);
-  vaddr_t addr = cpu.IDTR.base + 0x8 * NO;
+  vaddr_t addr = cpu.IDTR.base + 8 * NO;
   uint32_t high_bit = vaddr_read(addr+4,4) & 0xffff0000, low_bit = vaddr_read(addr,4)&0x0000ffff;//上层31.。16 以及 下层15.。0
   decoding.is_jmp = 1;
-  decoding.jmp_eip = high_bit + low_bit;
+  decoding.jmp_eip = high_bit|low_bit;
 }
 
 void dev_raise_intr() {
