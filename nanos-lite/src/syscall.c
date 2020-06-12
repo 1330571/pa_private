bd367906993e1a76c8327b14177dac17f466f7aa
xys <1422286969@qq.com>
2020-06-12 23:05:08 +0800

    PA3

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,8 +5,7 @@
 int mm_brk(uint32_t new_brk);
 
 static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t mode) {
-  TODO();
-  return 1;
+  return fs_open((char *)pathname, flags, mode);
 }
 
 static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
@@ -14,8 +13,7 @@ static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
 }
 
 static inline uintptr_t sys_read(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-  TODO();
-  return 1;
+  return fs_read(fd,(void *)buf,len);
 }
 
 static inline uintptr_t sys_lseek(uintptr_t fd, uintptr_t offset, uintptr_t whence) {
@@ -23,13 +21,11 @@ static inline uintptr_t sys_lseek(uintptr_t fd, uintptr_t offset, uintptr_t when
 }
 
 static inline uintptr_t sys_close(uintptr_t fd) {
-  TODO();
-  return 1;
+  return fs_close(fd);
 }
 
 static inline uintptr_t sys_brk(uintptr_t new_brk) {
-  TODO();
-  return 1;
+  return 0;
 }
 
 _RegSet* do_syscall(_RegSet *r) {
@@ -41,6 +37,7 @@ _RegSet* do_syscall(_RegSet *r) {
 
   switch (a[0]) {
     case SYS_write:{
+      // SYSCALL_ARG1(r) = sys_write(a[1],a[2],a[3]);
       SYSCALL_ARG1(r) = sys_write(a[1],a[2],a[3]);
       break;
     }
@@ -53,23 +50,28 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
     }
     case SYS_brk: {
-      SYSCALL_ARG1(r) = 0; //成功
+      // SYSCALL_ARG1(r) = 0; //成功
+      SYSCALL_ARG1(r) = sys_brk(a[1]);
       break;
     }
     case SYS_open: {
-      SYSCALL_ARG1(r) = fs_open((char *)a[1],(int)a[2],(int)a[3]);
+      // SYSCALL_ARG1(r) = fs_open((char *)a[1],(int)a[2],(int)a[3]);
+      SYSCALL_ARG1(r) = sys_open(a[1],a[2],a[3]);
       break;
     }
     case SYS_read: {
-      SYSCALL_ARG1(r) = fs_read((int)a[1],(void*)a[2],(size_t)a[3]);
+      // SYSCALL_ARG1(r) = fs_read((int)a[1],(void*)a[2],(size_t)a[3]);
+      SYSCALL_ARG1(r) = sys_read(a[1],a[2],a[3]);
       break;
     }
     case SYS_lseek: {
-      SYSCALL_ARG1(r) = fs_lseek((int)a[1],(off_t)a[2],(int)a[3]);
+      // SYSCALL_ARG1(r) = fs_lseek((int)a[1],(off_t)a[2],(int)a[3]);
+      SYSCALL_ARG1(r) = sys_lseek(a[1],a[2],a[3]);
       break;
     }
     case SYS_close: {
-      SYSCALL_ARG1(r) = fs_close((int)a[1]);
+      // SYSCALL_ARG1(r) = fs_close((int)a[1]);
+      SYSCALL_ARG1(r) = sys_close(a[1]);
       break;
     }
     default: panic("Unhandled syscall ID = %d", a[0]);
