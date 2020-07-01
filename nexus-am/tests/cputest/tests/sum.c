029638e77036e625a11da96e784b74a33607fc20
xys <1422286969@qq.com>
2020-07-01 09:51:52 +0800

    PA4

--- nexus-am/tests/cputest/tests/sum.c
+++ nexus-am/tests/cputest/tests/sum.c
@@ -1,16 +1,13 @@
 #include "trap.h"
 
 int main() {
-	// int i = 1;
-	// volatile int sum = 0;
-	// while(i <= 100) {
-	// 	sum += i;
-	// 	i ++;
-	// }
-
-	// nemu_assert(sum == 5050);
-	while(1){
-		printf("I am infinite loop\n");
+	int i = 1;
+	volatile int sum = 0;
+	while(i <= 100) {
+		sum += i;
+		i ++;
 	}
+
+	nemu_assert(sum == 5050);
 	return 0;
 }
